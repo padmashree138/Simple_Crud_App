a296bb084e6e18bebf148b0071284f8880d73ae9
Padmashree Petkar <156641444+padmashree138@users.noreply.github.com>
2024-03-03 03:17:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const { getProducts } = require("./controllers/product.controller.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://padmashree_petkar:AqzMsbis7C7wITxU@backenddb.bv6r17x.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to Database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
